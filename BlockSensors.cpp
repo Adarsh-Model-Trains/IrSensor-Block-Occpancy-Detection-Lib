80f9fecb35eff45ec956bc8e03b178ec02a7764b
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-03-13 19:27:36 -0600

    updating with validation

--- BlockSensors.cpp
+++ BlockSensors.cpp
@@ -9,18 +9,37 @@
 #include "BlockSensors.h"
 
 void BlockSensors::initBlockSensors(int totalSensor) {
-  if (totalSensor > 0)
-    _totalSensor = totalSensor;
-  this->_irSensorBlocks = new IrSensor[_totalSensor];
-  for (int i = 0; i < _totalSensor; i++) {
-    this->_irSensorBlocks[i].setStartAndEndBlockSensorPin(0, 0);
+  if (totalSensor > 0) {
+    if (totalSensor > 0)
+      _totalSensor = totalSensor;
+    this->_irSensorBlocks = new IrSensor[_totalSensor];
+    for (int i = 0; i < _totalSensor; i++) {
+      this->_irSensorBlocks[i].setStartAndEndBlockSensorPin(0, 0);
+    }
+  } else {
+    Serial.println("Invalid Sensor Count");
   }
 }
 
 void BlockSensors::setBlockSensorPins(int blockNo, int startBlockSensorPin, int endBlockSensorPin) {
-  _irSensorBlocks[blockNo - 1].setStartAndEndBlockSensorPin(startBlockSensorPin, endBlockSensorPin);
+  if (blockNo > -1 && blockNo <= _totalSensor ) {
+    if (startBlockSensorPin > -1 && endBlockSensorPin > -1) {
+      pinMode(startBlockSensorPin, INPUT);
+      pinMode(endBlockSensorPin, INPUT);
+      _irSensorBlocks[blockNo - 1].setStartAndEndBlockSensorPin(startBlockSensorPin, endBlockSensorPin);
+    } else {
+      Serial.println("Invalid Block Sensor Pin No.");
+    }
+  } else {
+    Serial.println("Invalid Block Sensor Input No.");
+  }
 }
 
 bool BlockSensors::isSensorBlockOccupied(int blockNo) {
-  return _irSensorBlocks[blockNo - 1].isBlockOccupied();
+  if (blockNo > -1 && blockNo <= _totalSensor) {
+    return _irSensorBlocks[blockNo - 1].isBlockOccupied();
+  } else {
+    Serial.println("Invalid Block Sensor No");
+    return false;
+  }
 }

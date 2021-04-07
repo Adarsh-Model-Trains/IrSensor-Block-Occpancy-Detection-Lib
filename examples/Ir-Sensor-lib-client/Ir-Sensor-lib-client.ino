8b65c6ea4278f0708592f37c662f64bd6e3387f6
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-04-07 15:01:42 -0500

    ready for production

--- examples/Ir-Sensor-lib-client/Ir-Sensor-lib-client.ino
+++ examples/Ir-Sensor-lib-client/Ir-Sensor-lib-client.ino
@@ -1,6 +1,8 @@
 
 #include "BlockSensors.h"
-#define NO_OF_BLOCKS 5
+#define NO_OF_BLOCKS 3
+
+int block_led[] = {7, 6, 5};
 
 BlockSensors blockSensors;
 
@@ -10,17 +12,26 @@ void setup() {
   blockSensors.setBlockSensorPins(1, 13, 12);
   blockSensors.setBlockSensorPins(2, 11, 10);
   blockSensors.setBlockSensorPins(3, 9, 8);
-  blockSensors.setBlockSensorPins(4, 7, 6);
-  blockSensors.setBlockSensorPins(5, 5, 4);
+  for (int i = 0; i < NO_OF_BLOCKS; i++) {
+    pinMode(block_led[i], OUTPUT);
+  }
 }
 
 void loop() {
-  delay(1000);
   for (int i = 1; i <= NO_OF_BLOCKS; i++) {
-    bool isBlockOccuipied = blockSensors.isSensorBlockOccupied(i);
-    Serial.print("Block no  ");
-    Serial.print(i);
-    Serial.print("  isBlockOccuipied  ");
-    Serial.println(isBlockOccuipied);
+    checkBlockStatus(i);
+  }
+}
+
+void checkBlockStatus(int blockNo) {
+  bool isBlockOccuipied = blockSensors.isSensorBlockOccupied(blockNo);
+  Serial.print("Block no  ");
+  Serial.print(blockNo);
+  Serial.print("  isBlockOccuipied  ");
+  Serial.println(isBlockOccuipied);
+  if (isBlockOccuipied) {
+    digitalWrite(block_led[blockNo - 1], HIGH);
+  } else {
+    digitalWrite(block_led[blockNo - 1], LOW);
   }
 }
